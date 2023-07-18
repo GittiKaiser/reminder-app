45a9dd0477442f56412fc09bbb9fc93b41df34f2
GittiHirt <hirt.felix98@web.de>
2023-07-18 14:46:51 +0200

    Style-Teil entfernt

--- reminder-app/src/views/HomePage.vue
+++ reminder-app/src/views/HomePage.vue
@@ -18,7 +18,7 @@
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
       
-      <button>Erinnerung hinzufügen</button>
+      <button>Erinnerung hinzufï¿½gen</button>
 
     </ion-content>
   </ion-page>
@@ -27,33 +27,3 @@
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
