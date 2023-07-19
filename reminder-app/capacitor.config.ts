78d0ed67be57278b6d98dfded5915364ce22474d
schul363 <lukas_bok@arburg.com>
2023-07-19 15:43:18 +0200

    Ich habe versucht die beiden Plug Ins (Local Notifications; Preferences)  hinzuzufügen. In Datei AndroidManifest.xml; build.grandle; capacitor.config.ts

--- reminder-app/capacitor.config.ts
+++ reminder-app/capacitor.config.ts
@@ -1,6 +1,31 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { Preferences } from '@capacitor/preferences';
+
+const setName = async () => {
+  await Preferences.set({
+    key: 'name',
+    value: 'Max',
+  });
+};
+
+const checkName = async () => {
+  const { value } = await Preferences.get({ key: 'name' });
+
+  console.log(`Hello ${value}!`);
+};
+
+const removeName = async () => {
+  await Preferences.remove({ key: 'name' });
+};
 
 const config: CapacitorConfig = {
+  plugins: {
+    LocalNotificatios: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    }
+  },
   appId: 'io.ionic.starter',
   appName: 'reminder-app',
   webDir: 'dist',
