7d5729769889b7ba108df0626926896f514ba541
schul363 <lukas_bok@arburg.com>
2023-07-19 15:56:15 +0200

    Habe auuch noch das optionale Plug-In Datetime Picker in Datei capacitor.config.ts hinzugefügt

--- reminder-app/capacitor.config.ts
+++ reminder-app/capacitor.config.ts
@@ -1,5 +1,27 @@
 import { CapacitorConfig } from '@capacitor/cli';
 import { Preferences } from '@capacitor/preferences';
+import { DatetimePicker } from '@capawesome-team/capacitor-datetime-picker';
+
+const present = async () => {
+  const date = new Date('1995-12-24T02:23:00');
+
+  const result = await DatetimePicker.present({
+    cancelButtonText: 'Cancel',
+    doneButtonText: 'Ok',
+    mode: 'time',
+    value: date.toISOString(),
+    theme: 'dark',
+    locale: 'en-US',
+  });
+
+  if (result && typeof result === 'object' && 'value' in result) {
+    return result.value;
+  }
+
+  return null; // Wenn die Eigenschaft "value" nicht vorhanden ist oder das Ergebnis null ist
+};
+
+
 
 const setName = async () => {
   await Preferences.set({
